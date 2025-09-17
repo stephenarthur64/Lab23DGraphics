75111ad5542c5d8e735c683b7b608a44297442fc
Ian-PB <C00294680@setu.ie>
2025-09-17 16:46:29 +0100

    Added bouncing

--- Lab2/Assets/Scripts/EnemyMovement.cs
+++ Lab2/Assets/Scripts/EnemyMovement.cs
@@ -6,9 +6,7 @@ public class EnemyMovement : MonoBehaviour
 {
     private Rigidbody rb;
     private Vector2 movementDir;
-    private int speed = 0;
-
-    public float scale = 1.0f;
+    public int speed = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -17,22 +15,22 @@ public class EnemyMovement : MonoBehaviour
 
         float randX = Random.Range(-1.0f, 1.0f);
         float randY = Random.Range(-1.0f, 1.0f);
-        speed = Random.Range(5, 10);
 
+        float scale = 1.0f;
         scale = Random.Range(0.5f, 1.0f);
         rb.gameObject.transform.localScale = new Vector3( scale, scale, scale );
 
         movementDir = new Vector2(randX, randY);
-    }
-
-    void FixedUpdate()
-    {
         Vector3 movement = new Vector3(movementDir.x, 0.0f, movementDir.y);
         rb.AddForce(movement * speed);
     }
 
-    void Kill()
+    void FixedUpdate()
     {
-        gameObject.SetActive(false);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
     }
 }
